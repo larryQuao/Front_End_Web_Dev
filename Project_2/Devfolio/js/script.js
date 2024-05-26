fa962d36dae53fba224a46c6b5a0efe2891fd6fd
larryq561 <131362406+larryQuao@users.noreply.github.com>
2024-05-26 03:59:24 +0100

    some update

--- Project_2/Devfolio/js/script.js
+++ Project_2/Devfolio/js/script.js
@@ -4,3 +4,26 @@
  */
 
 "use strict";
+
+/**
+ * Light and dark mode
+ */
+
+const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
+const /** {NodeElement} */ $HTML = document.documentElement;
+let /** Boolean | String */ isDark = window.matchMedia("(prefers-color-scheme)").matches;
+
+if (sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+const changeTheme = () => {
+
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme)
+
+}
+
+$themeBtn.addEventListener("click", changeTheme)
