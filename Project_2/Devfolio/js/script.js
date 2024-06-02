722603c771f372761d4a0f2d74926689cb6c6e77
LarryQuao <honyadjilawrence2s1@gmail.com>
2024-06-02 14:54:27 +0000

    upadtes

--- Project_2/Devfolio/js/script.js
+++ Project_2/Devfolio/js/script.js
@@ -5,31 +5,12 @@
 
 "use strict";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 6993ca4d1718775ff3a240160a040ffe5b4f7519
 /**
  * Light and dark mode
  */
 
 const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
 const /** {NodeElement} */ $HTML = document.documentElement;
-<<<<<<< HEAD
-let /** Boolean | String */ isDark = window.matchMedia("(prefers-color-scheme)").matches;
-
-if (sessionStorage.getItem("theme")) {
-    $HTML.dataset.theme = sessionStorage.getItem("theme");
-} else {
-    $HTML.dataset.theme = isDark ? "dark" : "light";
-}
-
-const changeTheme = () => {
-
-    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
-    sessionStorage.setItem("theme", $HTML.dataset.theme)
-
-=======
 let /** {Boolean | String} */ isDark =window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 // console.log(isDark)
@@ -45,7 +26,6 @@ else {
 const changeTheme = () => {
     $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
     sessionStorage.setItem("theme", $HTML.dataset.theme);
->>>>>>> 6993ca4d1718775ff3a240160a040ffe5b4f7519
 }
 
 $themeBtn.addEventListener("click", changeTheme)
